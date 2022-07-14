9ecf9e78a14409d4d156aeda27b1b8de10859ecb
Shreeraksha02 <109224850+Shreeraksha02@users.noreply.github.com>
2022-07-14 20:31:49 +0530

    14-07-2022

--- fact2.java
+++ fact2.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 public class factorial {
-    public static void main(String arg[]) {
-        int num, digit=0,temp;
+    public static void main(String arg[]) 
+    {
+        int number,temp=1;
+        long fact = 1;
         Scanner bs=new Scanner(System.in);
         System.out.println("enter a number to print its factorial");
-        num=bs.nextInt();//the number for which factorial value has to be found
-        temp=num;//temporary variable to calculate the value
-      
-        while(digit<=temp) 
+        number=bs.nextInt();
+        while(temp<=number)
         {
-            temp=temp*digit;
-            digit++;
+            fact = fact * temp;
+            temp++;
         }
-        System.out.println("Factorial of"+temp+"is"+digit);
+        System.out.println("Factorial of "+number+" is: "+fact);
     }
 }
