f747555bc8b449ef2b3db3d888a2bc442a429869
Shreeraksha02 <109224850+Shreeraksha02@users.noreply.github.com>
2022-08-12 10:27:17 +0530

    11/08/2022

---
+++ Libraryclass.java
@@ -0,0 +1,40 @@
+class Author{
+	 String authorName;
+	 String authorCountry;
+	public Author(String authorname,String authorcountry)
+	{
+		authorName=authorname;
+		authorCountry=authorcountry;
+	}
+}
+
+class Book{
+	int BookNum;
+	String BookName;
+	Author BookAuthor;//passing class reference to book class
+
+	public Book(int num,String book, Author atr)
+	{
+
+		BookNum=num;
+		BookName=book;
+		BookAuthor=atr;
+	}
+	//printing
+	public void bookPrint(){
+		System.out.println("Hello "+BookName+"book id :"+BookNum+" and author is "+BookAuthor.authorName);
+	}
+}
+
+class Libaray{
+	public static void main(String ...args)
+	{
+		//author object
+		Author a1=new Author("Kathy Seirra","UK");
+		//book object
+		Book b1=new Book(1234,"Java SCJP",a1);
+			b1.bookPrint();
+
+	}
+
+}
